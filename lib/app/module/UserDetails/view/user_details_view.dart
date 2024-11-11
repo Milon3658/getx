fd2ab9db22ad2f24e08bd565e2988968f405107a
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-11-11 18:23:13 +0600

    user details button

--- lib/app/module/UserDetails/view/user_details_view.dart
+++ lib/app/module/UserDetails/view/user_details_view.dart
@@ -10,20 +10,63 @@ class UserDetailsView extends GetView<UserDetailsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('User Details'),
-      ),
-      body: Text(
-        'Name: ${userDetails['name']} \n'
-        'Email: ${userDetails['email']} \n'
-        'Address: ${userDetails['address']} \n'
-        'City: ${userDetails['city']} \n'
-        'Country: ${userDetails['country']} \n',
-        style: const TextStyle(
-          fontSize: 20,
-          color: Colors.black,
+        appBar: AppBar(
+          title: const Text('User Details'),
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.amber.withOpacity(0.2),
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Name: ${userDetails['name']} \n'
+                    'Email: ${userDetails['email']} \n'
+                    'Address: ${userDetails['address']} \n'
+                    'City: ${userDetails['city']} \n'
+                    'Country: ${userDetails['country']}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.red),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Delete',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Add'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
