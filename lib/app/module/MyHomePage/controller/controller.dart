a55273ee77edf5ac32b4af115c271db38ea3a3e8
Jan Mahbub Milon <jan.milon.56@gmail.com>
2024-11-11 16:10:44 +0600

    loader for api call

--- lib/app/module/MyHomePage/controller/controller.dart
+++ lib/app/module/MyHomePage/controller/controller.dart
@@ -9,12 +9,15 @@ import 'package:testig_package/app/data/model/ContactModel.dart';
 
 class HomeController extends GetxController {
   Rx<ContactModel?> contacts = Rx(null);
+  RxBool isLoading = false.obs;
 
   Future<ContactModel> fetchUser() async {
+    isLoading.value = true;
     final response = await http.get(Uri.parse(baseUrl));
     try {
       final data = ContactModel.fromJson(jsonDecode(response.body));
       contacts.value = data;
+      isLoading.value = false;
       return data;
     } catch (e) {
       log(e.toString());
--- lib/app/module/MyHomePage/view/MyHomePage.dart
+++ lib/app/module/MyHomePage/view/MyHomePage.dart
@@ -17,52 +17,70 @@ class HomeView extends GetView<HomeController> {
         backgroundColor: Colors.amber,
         actions: [
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                controller.fetchUser();
+              },
               icon: const Icon(
                 Icons.refresh,
                 color: Colors.black,
               )),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(children: [
-            FutureBuilder(
-                future: controller.fetchUser(),
-                builder: (ctx, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(
-                        color: Colors.amber,
-                      ),
-                    );
-                  } else {
-                    if (snapshot.hasError) {
-                      return const Center(child: Text('Error'));
-                    } else {
-                      return ListView.builder(
-                        itemCount: snapshot.data?.profiles?.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (ctx, index) {
-                          return ContactCard(
-                            name: snapshot.data?.profiles?[index].name ?? '',
-                            email: snapshot.data?.profiles?[index].email ?? '',
-                            address:
-                                snapshot.data?.profiles?[index].address ?? '',
-                            city: snapshot.data?.profiles?[index].city ?? '',
-                            country:
-                                snapshot.data?.profiles?[index].country ?? '',
+      body: controller.isLoading.value == true
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(children: [
+                  FutureBuilder(
+                      future: controller.fetchUser(),
+                      builder: (ctx, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.amber,
+                              ),
+                            ),
                           );
-                        },
-                      );
-                    }
-                  }
-                })
-          ]),
-        ),
-      ),
+                        } else {
+                          if (snapshot.hasError) {
+                            return const Center(child: Text('Error'));
+                          } else {
+                            return Obx(
+                              () => ListView.builder(
+                                itemCount:
+                                    controller.contacts.value?.profiles?.length,
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemBuilder: (ctx, index) {
+                                  return ContactCard(
+                                    name: controller.contacts.value
+                                            ?.profiles?[index].name ??
+                                        '',
+                                    email: controller.contacts.value
+                                            ?.profiles?[index].email ??
+                                        '',
+                                    address: controller.contacts.value
+                                            ?.profiles?[index].address ??
+                                        '',
+                                    city: controller.contacts.value
+                                            ?.profiles?[index].city ??
+                                        '',
+                                    country: controller.contacts.value
+                                            ?.profiles?[index].country ??
+                                        '',
+                                  );
+                                },
+                              ),
+                            );
+                          }
+                        }
+                      })
+                ]),
+              ),
+            ),
     );
   }
 }
